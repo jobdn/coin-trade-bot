d4654f6bfb19ce22391f26c05191d0e7d7f969de
Danila Pisarev <jobdan@bk.ru>
2022-05-15 22:47:49 +0300

    feat: add the menu items

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import { Layout, Menu } from "antd";
+import { Layout, Menu, MenuProps } from "antd";
 import AppFooter from "./components/AppFooter";
 import AppHeader from "./components/AppHeader";
 
 import "./App.scss";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChartSimple, faUser } from "@fortawesome/free-solid-svg-icons";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[]
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+const items = [
+  getItem("User Account", 1, <FontAwesomeIcon icon={faUser} />),
+  getItem("Statistics", 2, <FontAwesomeIcon icon={faChartSimple} />),
+];
 
 function App() {
   const [collapsed, setCollapsed] = useState(true);
@@ -20,7 +43,7 @@ function App() {
         onCollapse={onCollapse}
         style={{ background: "#27273B", color: "#9F9F9F" }}
       >
-        <Menu />
+        <Menu items={items} theme="dark" />
       </Layout.Sider>
       <Layout>
         <AppHeader />
