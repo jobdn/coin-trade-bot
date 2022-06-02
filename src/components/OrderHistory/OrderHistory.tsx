3eee33c1f29679caa8f6c631c888db32c8c09398
Danila Pisarev <jobdan@bk.ru>
2022-06-02 17:56:30 +0300

    dependencies: add hardhat package and component for Orders

--- src/components/OrderHistory/OrderHistory.tsx
+++ src/components/OrderHistory/OrderHistory.tsx
@@ -1,9 +1,35 @@
 import React from "react";
 import { Row, Typography } from "antd";
+
+import axios, { AxiosResponse } from "axios";
+
 import styles from "./OrderHistory.module.scss";
 
+interface ITrending {
+  coins: ICoin[];
+}
+
+interface ICoin {
+  item: ICoinItem;
+}
+
+interface ICoinItem {
+  id: string;
+  coin_id: number;
+  name: string;
+  symbol: string;
+  market_cap_rank: number;
+  thumb: string;
+  small: string;
+  large: string;
+  slug: string;
+  price_btc: number;
+  score: number;
+}
+
 const OrderHistory = () => {
-  console.log(styles);
+  React.useEffect(() => {}, []);
+
   return (
     <>
       <Typography.Title level={2} className={styles["order-history-title"]}>
